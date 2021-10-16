9414bd1b17100b0ac2b52856df38c0eace279998
zelaya201 <mario.zelaya01@gmail.com>
2021-10-16 13:24:27 -0600

    Estructura del proyecto

---
+++ src/modelos/conexion/Conexion.java
@@ -0,0 +1,38 @@
+package modelos.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String user = "root";
+    private static final String password = "";
+    private static final String url = "jdbc:mysql://localhost:3306/db_hotel";  // ruta de la base
+
+    public static synchronized Connection getConexion() {
+        Connection cn = null;
+        try {
+            Class.forName(driver);
+            cn = DriverManager.getConnection(url, user, password);
+
+        } catch (Exception ex) {
+            System.out.println("Error en la conexion");
+            ex.printStackTrace();
+        }
+        return cn;
+    }
+
+    public static void closeConexion(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException sql) {
+            System.out.println("Error en la conexion");
+            sql.printStackTrace();
+        }
+    }
+
+}
---
+++ src/utilidades/NodoDoble.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidades;
+
+/**
+ *
+ * @author Mario Zelaya
+ */
+public class NodoDoble <T>{
+    T dato;
+    NodoDoble anterior;
+    NodoDoble siguiente;
+
+    public NodoDoble() {
+        
+    }
+    
+    public NodoDoble(T dato, NodoDoble anterior, NodoDoble siguiente) {
+        this.dato = dato;
+        this.anterior = anterior;
+        this.siguiente = siguiente;
+    }
+
+    public NodoDoble(T dato) {
+        this.dato = dato;
+        this.anterior = null;
+        this.siguiente = null;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public NodoDoble getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoDoble anterior) {
+        this.anterior = anterior;
+    }
+
+    public NodoDoble getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoDoble siguiente) {
+        this.siguiente = siguiente;
+    } 
+}
